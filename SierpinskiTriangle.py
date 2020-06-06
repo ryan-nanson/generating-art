20a9eb11f67d908bf471544b556163c3f72b9453
ryan-nanson <ryan_nanson@hotmail.com>
2020-06-06 14:16:48 +0100

    feat: complete sierpinski triangle code and gif

--- SierpinskiTriangle.py
+++ SierpinskiTriangle.py
@@ -1,65 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from itertools import product
+# Credit for the logic: https://www.youtube.com/watch?v=IRY1HoU2Qo0
+
 from PIL import Image, ImageDraw
 import random
-import math
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-def save_animated_gif(filename, images, durations):
-    """
-    Save images as frames of an animated GIF.  Durations should specify the
-    milliseconds to display each frame, and should be of the same length as
-    images.
-    """
-    # https://pillow.readthedocs.io/en/latest/handbook/image-file-formats.html#saving
-    first_image, *other_images = images
-    backwards_images = other_images[::-1] #reversing using list slicing
-    all_images = other_images + backwards_images + [first_image]
-    durations = durations + durations
-    first_image.save(filename, save_all=True, append_images=all_images, duration=durations, loop=0)
 
-def punch_hole(draw, x, y, length, hole_color):
+def sierpinkspi(p1, p2, p3, degree, draw, image, colors):
     """
-    For a triangle with a point at (x, y) and sides of length section_size,
-    divide it into 4 tiles, and fill the center tile with background_color.
+    Draw Sierpinksi Triangles.
     """
-    bottom_point = (x, y + math.sqrt(3) * length/2)
-    left_point = (x - length/4, y + math.sqrt(3) * length/4)
-    right_point = (x + length/4, y + math.sqrt(3) * length/4)
-    draw.polygon((bottom_point, left_point, right_point), fill=hole_color)
-
-def make_triangles(n, triangle_color, background_color):
-    """
-    Generate n PIL Images, each of Sierpiński's triangle with increasing levels
-    of detail.
-    """
-    image_width = 690
-    image_height = 600   
-    image = Image.new("RGBA", (image_width, image_height), background_color)
-    yield image
-    image = image.copy()
-    draw = ImageDraw.Draw(image)
+    colour = colors
+    draw.polygon(((p1[0], p1[1]), (p2[0], p2[1]), (p3[0], p3[1])), fill=colour[degree])
     
-    # draw first triangle
-    draw.polygon([(0, image_height), (image_width/2, 0), (image_width,image_height)], fill = triangle_color)
-    yield image
-    image = image.copy()
-    draw = ImageDraw.Draw(image)
-    # draw frist hole
-    punch_hole(draw, image_width/2, 0, image_width, background_color)
-    image_width = image_width/2
-    yield image
-    for i in [-1, 0, 1]:
-        image = image.copy()
-        draw = ImageDraw.Draw(image)
-        punch_hole(draw, image_width + (i*image_width/2), abs(i) * (math.sqrt(3) * image_width/2), image_width, background_color)
-    yield image
+    if degree > 0:
+        sierpinkspi(p1, mid(p1, p2), mid(p1, p3), degree-1, draw, image, colors)
+        sierpinkspi(p2, mid(p1, p2), mid(p2, p3), degree-1, draw, image, colors)
+        sierpinkspi(p3, mid(p1, p3), mid(p2, p3), degree-1, draw, image, colors)
+        
+    else:
+        return image
+    
+def mid(p1, p2):
+    """
+    Get mid point of two points.
+    """
+    return [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
 
+def triangle(p1, p2, p3, colour, draw):
+    """
+    Draw a single Triangle.
+    """
+    logger.info(f"triangle:, {p1}, {p2}, {p3}")
+    draw.polygon(((p1[0], p1[1]), (p2[0], p2[1]), (p3[0], p3[1])), fill=colour)
+    return draw
         
 def random_color():
     """
@@ -68,16 +47,49 @@ def random_color():
     r = lambda: random.randint(0,255)
     return('#%02X%02X%02X' % (r(),r(),r()))
 
+def save_animated_gif(filename, images, durations):
+    """
+    Save images as frames of an animated GIF. Durations should specify the
+    milliseconds to display each frame, and should be of the same length as
+    images.
+    """
+    #set up images and times to play GIF forward and back
+    first_image, *other_images = images
+    backwards_images = other_images[::-1]
+    all_images = other_images + backwards_images + [first_image]
+    durations = durations + durations
+
+    logger.info("Creating GIF")
+    first_image.save(fp="SierpinskiTriangle.gif", format='GIF', save_all=True, append_images=all_images, duration=durations, loop=0)
+
 def main():
-    N = 7
+    degree = 6
+    image_width = 690
+    image_height = 600
+    frames = []
     
+    # set up colors for images and gif 
     triangle_color = random_color()
     background_color = random_color()
-    
-    triangles = make_triangles(N, triangle_color=triangle_color, background_color=background_color)
-    durations = [800] * 6
+    colors = [background_color] * (degree - 1)
+    colors.insert(0, triangle_color)
 
-    save_animated_gif("SierpinskiTriangle.gif", triangles, durations)
+    # draw outer triangle
+    p1 = [0, image_height]
+    p3 = [image_width/2, 0]
+    p2 = [image_width, image_height]
+    
+    # draw image for each layer of the Sierpinski Triangle
+    for i in range(0, degree):
+        logger.info(f"iteration: {i}")
+        image = Image.new("RGBA", (690, 600), background_color)
+        draw = ImageDraw.Draw(image) 
+        sierpinkspi(p1, p2, p3, i, draw, image, colors)
+        frames.append(image)
+    
+    # set up times for gif
+    durations = [800] * degree
+    save_animated_gif("SierpinskiTriangle.gif", frames, durations)
 
 if __name__ == '__main__':
     main()
